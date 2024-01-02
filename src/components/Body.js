0178739130e46926e5922362601cb58c4332a3eb
Vishal <vishalpal1501@gmail.com>
2024-01-03 00:04:04 +0530

    react-router-dom setup

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Login from './Login';
 import Browse from './Browse';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 function Body() {
+  const appRouter = createBrowserRouter([
+    { path: '/', element: <Login /> },
+    { path: '/browse', element: <Browse /> },
+  ]);
   return (
     <>
-      <Login />
-      <Browse />
+      <RouterProvider router={appRouter} />
     </>
   );
 }
