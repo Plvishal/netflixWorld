d429957e581dd5d7b3f387aa9def7f9a772662c8
Vishal <vishalpal1501@gmail.com>
2024-01-02 23:58:45 +0530

    page setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+
+import Body from './components/Body';
 
 function App() {
-  return <div className="text-3xlfont-bold text-green-800">NetflixGPT</div>;
+  return (
+    <>
+      <Body />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './Login';
+import Browse from './Browse';
+
+function Body() {
+  return (
+    <>
+      <Login />
+      <Browse />
+    </>
+  );
+}
+
+export default Body;
