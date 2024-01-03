0a464dfd3bb541bfb2bf0bdfa92ca99816e72474
Vishal <vishalpal1501@gmail.com>
2024-01-04 00:23:02 +0530

    sign in done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
-import React, { cloneElement, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './Header';
 import { checkValidData } from '../utils/validation';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { auth } from '../utils/firebase';
 
 function Login() {
@@ -37,7 +40,21 @@ function Login() {
         });
     } else {
       // sign in logic
-      console.log('you enter login page');
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + '-' + errorMessage);
+        });
     }
   };
   const handleSubmit = (e) => {
