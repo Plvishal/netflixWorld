7c0bd140240b0662560364916039a1d8bbceea03
Vishal <vishalpal1501@gmail.com>
2024-01-03 12:59:58 +0530

    get input data via useRef Hooks

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './Header';
+import { checkValidData } from '../utils/validation';
 
 function Login() {
   const [isSignUpForm, setIsSignUpForm] = useState();
+  const email = useRef(null);
+  const password = useRef(null);
   const toggleSignUpForm = () => {
     setIsSignUpForm(!isSignUpForm);
+  };
+  const handleButtonCLick = () => {
+    // checkValidData(email, password);
+    console.log(email, password);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
   return (
     <>
@@ -16,7 +26,10 @@ function Login() {
             alt="bg-full"
           />
         </div>
-        <form className=" w-3/12 absolute  p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg">
+        <form
+          className=" w-3/12 absolute  p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg"
+          onSubmit={handleSubmit}
+        >
           <h1 className="font-bold text-3xl py-4 ">
             {isSignUpForm ? 'Sign In' : 'Sign Up'}
           </h1>
@@ -32,13 +45,18 @@ function Login() {
             type="text"
             placeholder="Email Address"
             className="p-4 my-4 w-full bg-gray-500 rounded-lg"
+            ref={email}
           />
           <input
+            ref={password}
             type="password"
             placeholder="Email Address"
             className="p-4 my-4 w-full bg-gray-500 rounded-lg"
           />
-          <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          <button
+            className="p-4 my-6 bg-red-700 w-full rounded-lg"
+            onClick={handleButtonCLick}
+          >
             {isSignUpForm ? 'Sign In' : 'Sign Up'}
           </button>
           <p className="py-4 cursor-pointer" onClick={toggleSignUpForm}>
